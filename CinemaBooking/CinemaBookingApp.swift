2781d66e7b9e4459a1f04371f9701065bc407c3a
Nick Rice <nick.rice@starlingbank.com>
2022-10-10 17:51:36 +0100

    Initial Commit

---
+++ CinemaBooking/CinemaBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaBookingApp.swift
+//  CinemaBooking
+//
+//  Created by Nick Rice on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CinemaBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
