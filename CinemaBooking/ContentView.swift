7376a1725aa3278be387f11a2841b50846cc38b8
Nick Rice <nick.rice@starlingbank.com>
2022-10-24 18:35:27 +0100

    - Custom Tab Bar

--- CinemaBooking/ContentView.swift
+++ CinemaBooking/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentTab: Tab = .home
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
-        TicketView()
-            .statusBarHidden()
+        VStack(spacing: 0.0) {
+            TabView(selection: $currentTab) {
+                Text("Home")
+                    .tag(Tab.home)
+                
+                Text("Location")
+                    .tag(Tab.location)
+                
+                TicketView()
+                    .tag(Tab.ticket)
+                
+                Text("Category")
+                    .tag(Tab.category)
+                
+                Text("Profile")
+                    .tag(Tab.profile)
+            }
+            
+            CustomTabBar(currentTab: $currentTab)
+        }
     }
 }
 
---
+++ CinemaBooking/Data/TabData.swift
@@ -0,0 +1,16 @@
+//
+//  TabData.swift
+//  CinemaBooking
+//
+//  Created by Nick Rice on 24/10/2022.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable {
+    case home = "Home"
+    case location = "Location"
+    case ticket = "Ticket"
+    case category = "Category"
+    case profile = "Profile"
+}
